8f77e10596cf54fb951aec66193fa6d062d78df7
samueljustus <samie.justus@gmail.com>
2022-12-12 04:10:29 -0800

    size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-printf("size of char is=%i byte\n", sizeof(char));
-printf("size of int is=%i bytes\n", sizeof(int));
-printf("size of long int is=%i bytes\n", sizeof(long int));
-printf("size of long long int is=%i bytes\n", sizeof(long long int));
-printf("size of float is=%i bytes\n", sizeof(float));
+printf("size of char is=%i byte(s)\n", sizeof(char));
+printf("size of int is=%i byte(s)\n", sizeof(int));
+printf("size of long int is=%i byte(s)\n", sizeof(long int));
+printf("size of long long int is=%i byte(s)\n", sizeof(long long int));
+printf("size of float is=%i byte(s)\n", sizeof(float));
 return (0);
 }
 
