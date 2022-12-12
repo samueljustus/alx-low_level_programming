198617627022df75a2b043c750bae53304733e30
samueljustus <samie.justus@gmail.com>
2022-12-12 06:54:47 -0800

    all projects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main- Serves as the entry point for the program
  *
- * Return: always 0 (success)
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-c programming basics
+Readme
