1492f7d1d29210f6313520febe44cedc21c5f56c
samueljustus <samie.justus@gmail.com>
2022-12-12 02:17:03 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - main block
- *return: 0
+ * Return: 0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,/n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
