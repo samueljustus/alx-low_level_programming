15e27cdf4816d6198ee7255753f11a4d87ce598a
samueljustus <samie.justus@gmail.com>
2022-12-14 11:30:06 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,17 +8,6 @@ include "main.h"
 int _isalpha(int c)
 
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-	{
-		return (1);
-
-	}
-	else
-
-	{
-		return (0);
-
-	}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
 }
