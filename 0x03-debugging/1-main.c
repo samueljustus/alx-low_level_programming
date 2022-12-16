9e03b68eafb33e71d26b3a694b2550f06ab9b1c6
samueljustus <samie.justus@gmail.com>
2022-12-16 06:46:01 -0800

    infinte loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,14 +14,14 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	/*
+	 *while (i < 10)
+	 *{
+	 *putchar(1)
+	 *}
+	 */
 
-	{
-		putchar(i);
-
-	}
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 
 }
