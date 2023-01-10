4196f903ae67e3e8eabf9d65dca8f5a44b19a6eb
samueljustus <samie.justus@gmail.com>
2023-01-10 07:30:30 -0800

    my main.h file

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
